c66ac5b6202c717b6ee802a97183b60a409c5cb6
Your Name <you@example.com>
2024-10-02 22:19:06 +0800

    KICKCO

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+use menu::MenuItem;
 use shapes::Drawable;
 
 mod fluent;
-mod shapes;
-mod menu;
 mod math;
+mod menu;
+mod shapes;
 
 pub trait Updateable {
     fn update(&mut self, delta: f32);
@@ -15,7 +16,72 @@ pub struct BoundlessDemo {
 
 impl BoundlessDemo {
     pub fn new() -> Self {
-        let options = vec!["Option 1".to_string(), "Option 2".to_string(), "Option 3".to_string()];
+        let options = vec![
+            "File".into(),
+            MenuItem::new(
+                "Edit".into(),
+                vec![
+                    "Undo".into(),
+                    "Redo".into(),
+                    MenuItem::new(
+                        "Advanced".into(),
+                        vec![
+                            "Cut".into(),
+                            "Copy".into(),
+                            "Paste".into(),
+                            MenuItem::new(
+                                "Special".into(),
+                                vec![
+                                    "Format".into(),
+                                    "Spell Check".into(),
+                                ],
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+            MenuItem::new(
+                "View".into(),
+                vec![
+                    "Zoom In".into(),
+                    "Zoom Out".into(),
+                    "FullScreen".into(),
+                    MenuItem::new(
+                        "Themes".into(),
+                        vec![
+                            "Light".into(),
+                            "Dark".into(),
+                            "Sepia".into(),
+                        ],
+                    ),
+                ],
+            ),
+            "Help".into(),
+            MenuItem::new(
+                "Settings".into(),
+                vec![
+                    "General".into(),
+                    "Appearance".into(),
+                    "Shortcuts".into(),
+                    MenuItem::new(
+                        "Advanced".into(),
+                        vec![
+                            "Network".into(),
+                            "Security".into(),
+                            "Privacy".into(),
+                            MenuItem::new(
+                                "Developer".into(),
+                                vec![
+                                    "Console".into(),
+                                    "Debug".into(),
+                                    "Logs".into(),
+                                ],
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+        ];
         Self {
             menu: menu::Menu::new(options),
         }
--- src/menu.rs
+++ src/menu.rs
@@ -1,4 +1,9 @@
-use std::{f32::consts::PI, option};
+use std::{
+    f32::consts::PI,
+    ops::Deref,
+    sync::{Arc, LazyLock, Mutex},
+    usize,
+};
 
 use macroquad::prelude::*;
 
@@ -9,38 +14,135 @@ use crate::{
     Updateable,
 };
 
-struct MenuItem {
+static STACK: LazyLock<Arc<Mutex<Vec<Vec<MenuItem>>>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(Vec::new())));
+
+#[derive(Clone)]
+pub struct MenuItem {
     text: String,
     sub: Vec<MenuItem>,
 }
 
+impl MenuItem {
+    pub fn new(text: String, sub: Vec<MenuItem>) -> Self {
+        Self { text, sub }
+    }
+}
+
+impl From<&str> for MenuItem {
+    fn from(s: &str) -> Self {
+        Self::new(s.to_string(), Vec::new())
+    }
+}
+
+impl Deref for MenuItem {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.text
+    }
+}
+
+#[derive(Clone)]
 pub struct Menu {
-    options: Vec<String>,
+    options: Vec<MenuItem>,
     lines: Vec<Fluent<LineFromOrigin>>,
 }
 
 impl Menu {
-    pub fn new(options: Vec<String>) -> Self {
-        let mut lines = Vec::new();
-        let len = options.len();
+    pub fn new(options: Vec<MenuItem>) -> Self {
+        let lines = Vec::new();
+
+        let mut s = Self { options, lines };
+        s.initialize(0.);
+
+        s
+    }
+
+    fn initialize(&mut self, rotation_delta: f32) {
+        let len = self.options.len();
         for i in 0..len {
-            let mut obj = Fluent::new(LineFromOrigin::new(0., 2., BLACK), 1.);
+            let mut obj = Fluent::new(LineFromOrigin::new(rotation_delta, 2., BLACK), 1.);
 
-            obj.set_target(LineFromOrigin::new(
-                2. * PI / len as f32 * i as f32,
-                2.,
-                BLACK,
-            ));
+            let mut rotation = 2. * PI / len as f32 * i as f32 + rotation_delta;
 
-            lines.push(obj);
+            while rotation < 0. {
+                rotation += 2. * PI;
+            }
+
+            while rotation > 2. * PI {
+                rotation -= 2. * PI;
+            }
+
+            obj.set_target(LineFromOrigin::new(rotation, 2., BLACK));
+
+            self.lines.push(obj);
+        }
+    }
+
+    fn calculate_click(&mut self, mouse_pos: (f32, f32)) -> Option<usize> {
+        let screen_center = vec2(screen_width() / 2., screen_height() / 2.);
+        let mouse_pos = vec2(mouse_pos.0, mouse_pos.1);
+
+        if (mouse_pos - screen_center).length() <= 50. {
+            return Some(usize::MAX);
         }
 
-        Self { options, lines }
+        let mut rotation = (mouse_pos - screen_center).to_angle();
+        if rotation < 0. {
+            rotation += PI * 2.;
+        }
+
+        for i in 0..self.lines.len() {
+            let line = &self.lines[i];
+
+            let begin = line.value.rotation();
+            let mut end = self.lines[(i + 1) % self.lines.len()].value.rotation();
+
+            if end <= 0. {
+                end += PI * 2.;
+            }
+
+            if begin <= rotation && rotation <= end {
+                return Some(i);
+            }
+        }
+
+        None
     }
 }
 
 impl Updateable for Menu {
     fn update(&mut self, delta: f32) {
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let mouse_pos = mouse_position();
+            if let Some(x) = self.calculate_click(mouse_pos) {
+
+                let rotation;
+
+                if x == usize::MAX {
+                    self.options = match STACK.lock().unwrap().pop() {
+                        Some(x) => x,
+                        None => return,
+                    };
+                    rotation = 0.;
+                } else if self.options[x].sub.is_empty() {
+                    return;
+                } else {
+                    STACK.lock().unwrap().push(self.options.clone());
+                    self.options = self.options[x].sub.clone();
+                    rotation = self.lines[x].value.rotation()
+                }
+
+                self.lines.clear();
+
+
+                self.initialize(rotation);
+
+                return;
+            }
+        }
+
         for line in self.lines.iter_mut() {
             line.update(delta);
         }
@@ -49,6 +151,10 @@ impl Updateable for Menu {
 
 impl Drawable for Menu {
     fn draw(&self) {
+        // Draw Back Button in the center with radius 50.
+        let center = vec2(screen_width() / 2., screen_height() / 2.);
+        draw_circle(center.x, center.y, 50., BLACK);
+
         for i in 0..self.options.len() {
             self.lines[i].value.draw();
             // Draw text
